f53f4cc23e72eb62ccc97f47838316161fc57d44
Mehul Suthar <mehulsuthar858@gmail.com>
2025-07-20 11:51:52 +0530

    Initial commit

---
+++ src/main/java/com/mehul/dbMigration/project/DbMigrationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mehul.dbMigration.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbMigrationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbMigrationProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mehul/dbMigration/project/DbMigrationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehul.dbMigration.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbMigrationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
